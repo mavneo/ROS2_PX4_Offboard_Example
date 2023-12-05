19f51dd505110aa07e59f9340900582db3a643ef
Neo <147580558+mavneo@users.noreply.github.com>
2023-12-05 17:55:31 +0530

    Update processes.py

--- px4_offboard/px4_offboard/processes.py
+++ px4_offboard/px4_offboard/processes.py
@@ -20,7 +20,7 @@ commands = [
     #"cd ~/ws && source install/setup.bash && ros2 run ros_gz_bridge parameter_bridge lidar@sensor_msgs/msg/LaserScan@ignition.msgs.LaserScan"
 
     #Run ros_gz_bridge
-    "cd ~/ws && source install/setup.bash && ros2 run ros_gz_bridge parameter_bridge /lidar/points@sensor_msgs/msg/PointCloud2@ignition.msgs.PointCloudPacked"    
+    "cd ~/workspace/ROS_GZ && ros2 run ros_gz_bridge parameter_bridge /lidar/points@sensor_msgs/msg/PointCloud2@ignition.msgs.PointCloudPacked"    
     
 ]
 
